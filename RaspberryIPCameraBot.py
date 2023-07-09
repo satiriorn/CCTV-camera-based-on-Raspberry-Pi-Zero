f595908cc1c58b9a052b1a5e0a97de1656179241
satiriorn <45328793+satiriorn@users.noreply.github.com>
2023-07-09 19:38:27 +0300

    added camera supprot and convert video to mp4

--- RaspberryIPCameraBot.py
+++ RaspberryIPCameraBot.py
@@ -4,8 +4,11 @@ import pytz
 from telegram.ext import Updater, MessageHandler, Filters
 import os
 import Thread
+import picamera
+from subprocess import call
 
 pir = MotionSensor(4)
+
 
 class RaspberryPiBot(object):
     _instance = None
@@ -19,6 +22,8 @@ class RaspberryPiBot(object):
         self.UseCommand = {}
         self.updater = Updater(os.getenv("TOKEN"), use_context=True)
         self.dispatcher = self.updater.dispatcher
+        self.camera = picamera.PiCamera()
+        self.camera.resolution = (640, 480)
         self.CreateHandler()
         self.run()
 
@@ -42,6 +47,19 @@ class RaspberryPiBot(object):
             now = datetime.now(pytz.timezone('Europe/Kyiv'))
             current_time = now.strftime("%H:%M:%S")
             print("Motion detected! Current Time =", current_time)
-            self.updater.bot.send_message(os.getenv("Chat_id"), "Motion detected! Current Time ="+str(current_time))
+            self.updater.bot.send_message(os.getenv("CHAT_ID"), ("Motion detected! Current Time =", current_time))
+            self.file_name = "/home/pi/Desktop/Video/" + now.strftime("%d-%m-%Y-%H-%M-%S") + ".h264"
+            self.camera.start_recording(self.file_name)
+            self.camera.wait_recording(20)
+            self.camera.stop_recording()
+            self.convert_video()
+            self.updater.bot.send_video(os.getenv("CHAT_ID"), open(self.file_name, 'rb'))
+
+    def convert_video(self):
+        file_mp4 = self.file_name.replace(".h264", ".mp4")
+        command = "MP4Box -add " + self.file_name + " " + file_mp4
+        call([command], shell=True)
+        self.file_name = file_mp4
+
 
 RaspberryPiBot()
